82ff6cc3f9f0254356c3cd24bc85f5ee7986cc58
aurelwidyanti <aurelwyt@gmail.com>
2024-08-13 08:12:17 +0700

    refactor: Add profile detail widget in profile screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:attendance_app/widgets/custom_navigation_bar.dart';
+import 'package:attendance_app/widgets/custom_profile_detail.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -65,38 +66,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
           ),
           const SizedBox(height: 40),
           // Profile Details
-          Container(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 24,
-              vertical: 12,
-            ),
-            margin: const EdgeInsets.symmetric(horizontal: 24),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(20),
-              border: Border.all(
-                color: const Color.fromRGBO(191, 191, 191, 1),
-                width: 1,
-              ),
-              boxShadow: const [
-                BoxShadow(
-                  color: Colors.black12,
-                  blurRadius: 2,
-                  spreadRadius: 0,
-                ),
-              ],
-            ),
-            child: const Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                ProfileDetailRow(label: 'Nama Mahasiswa', value: 'Lumine'),
-                ProfileDetailRow(label: 'NIM', value: 'A11.2022.14494'),
-                ProfileDetailRow(label: 'Status', value: 'Aktif'),
-                ProfileDetailRow(
-                    label: 'Dosen Wali', value: 'FARUZAN S.Kom, M.Kom'),
-              ],
-            ),
-          ),
+          const CustomProfileDetail(),
           const SizedBox(height: 40),
           // Logout Button
           ElevatedButton(
@@ -128,45 +98,6 @@ class _ProfileScreenState extends State<ProfileScreen> {
       bottomNavigationBar: CustomNavigationBar(
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
-      ),
-    );
-  }
-}
-
-class ProfileDetailRow extends StatelessWidget {
-  final String label;
-  final String value;
-
-  const ProfileDetailRow({required this.label, required this.value, Key? key})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 5.0),
-      child: Row(
-        children: <Widget>[
-          Text(
-            '$label ',
-            style: const TextStyle(
-              fontSize: 16,
-              color: Colors.grey,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-          const SizedBox(width: 10),
-          Expanded(
-            child: Text(
-              value,
-              style: const TextStyle(
-                fontSize: 16,
-                color: Colors.black,
-                fontWeight: FontWeight.w500,
-              ),
-              textAlign: TextAlign.right,
-            ),
-          ),
-        ],
       ),
     );
   }
