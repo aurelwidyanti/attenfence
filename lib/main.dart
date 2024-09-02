eabf89932167fda92422082f1c7f6faf694c6e7f
aurelwidyanti <aurelwyt@gmail.com>
2024-09-02 18:25:54 +0700

    feat: Add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,15 @@ import 'package:attendance_app/screens/splash_screen.dart';
 import 'package:attendance_app/widgets/custom_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(MaterialApp(
     title: 'Attendance',
     theme: ThemeData(
