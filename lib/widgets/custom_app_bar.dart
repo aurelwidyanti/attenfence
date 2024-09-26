621589468ca4483c5c26829434d0277c493fa3be
aurelwidyanti <aurelwyt@gmail.com>
2024-09-26 19:04:16 +0700

    refactor: Add width and title alignment in CustomAppBar

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -2,23 +2,35 @@ import 'package:flutter/material.dart';
 
 class CustomAppBar extends StatelessWidget {
   final String title;
-  const CustomAppBar({Key? key, required this.title}) : super(key: key);
+  final double width;
+  final Alignment titleAlignment;
+
+  const CustomAppBar({
+    Key? key,
+    required this.title,
+    required this.width,
+    this.titleAlignment = Alignment.center,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      backgroundColor: Colors.white,
-      elevation: 4,
-      shadowColor: Colors.white,
-      surfaceTintColor: Colors.transparent,
-      title: Center(
-        child: Text(
-          title,
-          style: const TextStyle(
-            color: Colors.black,
-            fontSize: 20,
-            fontWeight: FontWeight.w700,
-            letterSpacing: 0.5,
+    return SizedBox(
+      width: width,
+      child: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 4,
+        shadowColor: Colors.grey.withOpacity(0.25),
+        surfaceTintColor: Colors.transparent,
+        title: Align(
+          alignment: titleAlignment,
+          child: Text(
+            title,
+            style: const TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+              fontWeight: FontWeight.w700,
+              letterSpacing: 0.5,
+            ),
           ),
         ),
       ),
